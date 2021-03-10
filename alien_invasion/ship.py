38efcd38d02d0afa3667167478b93211f9bf519d
Amirh. Zahmatkesh Kenari <zkkamir@gmail.com>
2021-03-10 18:39:01 +0330

    Added the player's ship.

---
+++ alien_invasion/ship.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Ship:
+    """A class to manage the player's ship."""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set it's starting position."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get it's rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rest.midbottom
+    
+    def blitme(self):
+        """Draw the ship at it's current location"""
+        self.screen.blit(self.image, self.rect)
