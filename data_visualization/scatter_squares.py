d4295a8eb89b765a6fc637e478bc685dee6f6dee
Amirh. Zahmatkesh Kenari <zkkamir@gmail.com>
2021-04-01 12:28:13 +0430

    Played around with coloring and presentation.

--- data_visualization/scatter_squares.py
+++ data_visualization/scatter_squares.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 
 x_values = range(1, 1001)
-y_values = [x**2 for x in x_values]
+y_values = [x ** 2 for x in x_values]
 
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
