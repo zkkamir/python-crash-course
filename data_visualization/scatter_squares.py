4b1e9537260736e90b046121b7360e7a82f1c960
Amirh. Zahmatkesh Kenari <zkkamir@gmail.com>
2021-04-01 10:54:40 +0430

    Using a Colormap.

--- data_visualization/scatter_squares.py
+++ data_visualization/scatter_squares.py
@@ -6,7 +6,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -19,4 +19,4 @@ ax.tick_params(axis="both", which="major", labelsize=14)
 # Set the range for each axis.
 ax.axis([0, 1100, 0, 1100000])
 
-plt.savefig("mygraph.png")
+plt.savefig("mygraph.png", bbox_inches="tight")
