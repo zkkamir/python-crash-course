20cc458f7abda538540f2d6f8347a0ecec5cd3ab
Amirh. Zahmatkesh Kenari <zkkamir@gmail.com>
2021-04-03 16:53:56 +0430

    Added low temperatures to the graph.

--- data_visualization/sitka_highs_lows.py
+++ data_visualization/sitka_highs_lows.py
@@ -4,26 +4,30 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
-filename = "data/sitka_weather_07-2018_simple.csv"
+filename = "data/sitka_weather_2018_simple.csv"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    # Get dates and high temperatures from this file.
-    dates, highs = [], []
+    # Get dates, and high and low temperatures from this file.
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
         high = int(row[5])
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 
-# Plot the high temperatures.
+# Plot the high and low temperatures.
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c="red")
+ax.plot(dates, highs, c="red", alpha=0.5)
+ax.plot(dates, lows, c="blue", alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
 # Format plot.
-plt.title("Daily high temperatures, july 2018", fontsize=24)
+plt.title("Daily high and low temperatures - 2018", fontsize=24)
 plt.xlabel("", fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
