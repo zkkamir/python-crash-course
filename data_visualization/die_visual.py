2eb2e3c75706d9a966832fa5d2618ca27346f393
Amirh. Zahmatkesh Kenari <zkkamir@gmail.com>
2021-04-03 11:41:03 +0430

    Tried two die histogram and refactored for loops.

--- data_visualization/die_visual.py
+++ data_visualization/die_visual.py
@@ -4,30 +4,32 @@ from plotly import offline
 from die import Die
 
 
-# Create a D6
-die = Die()
+# Create a D6 and a D10.
+die_1 = Die()
+die_2 = Die(10)
 
 # Make some rolls, and store results in a list.
-results = []
-for roll_num in range(1000):
-    result = die.roll()
-    results.append(result)
+results = [die_1.roll() + die_2.roll() for roll_num in range(50_000)]
+# for roll_num in range(50_000):
+#     result = die_1.roll() + die_2.roll()
+#     results.append(result)
 
 # Analyze the results.
-frequencies = []
-for value in range(1, die.num_sides + 1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
+max_result = die_1.num_sides + die_2.num_sides
+frequencies = [results.count(value) for value in range(2, max_result + 1)]
+# for value in range(2, max_result + 1):
+#     frequency = results.count(value)
+#     frequencies.append(frequency)
 
 # Visualize the results.
-x_values = list(range(1, die.num_sides + 1))
+x_values = list(range(2, max_result + 1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {"title": "Result"}
+x_axis_config = {"title": "Result", "dtick": 1}
 y_axis_config = {"title": "Frequency of Result"}
 my_layout = Layout(
-    title="Result of rolling one D6 1000 times",
+    title="Result of rolling a D6 and a D10 50000 times",
     xaxis=x_axis_config,
     yaxis=y_axis_config,
 )
-offline.plot({"data": data, "layout": my_layout}, filename="d6.html")
+offline.plot({"data": data, "layout": my_layout}, filename="d6_d10.html")
