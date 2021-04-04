42f6f847b2a93d8d1ad11fbbe2ad03958d03009f
Amirh. Zahmatkesh Kenari <zkkamir@gmail.com>
2021-04-04 19:25:26 +0430

    Started a Django project and worked on learning_logs app.

---
+++ learning_log/learning_logs/models.py
@@ -0,0 +1,33 @@
+from typing import Text
+from django.db import models
+
+
+class Topic(models.Model):
+    """A class for a topic the user is learning about"""
+
+    text = models.CharField(max_length=255)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+
+class Entry(models.Model):
+    """
+    A class for something specific that the user learned about the topic
+    """
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        """A string representation of the model."""
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return f"{self.text}"
