2d2ab302e64ed2681ca04840b853debd6687c7c2
Amirh. Zahmatkesh Kenari <zkkamir@gmail.com>
2021-04-05 19:50:41 +0430

    Created 'edit entry' page.

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -17,4 +17,6 @@ urlpatterns = [
     path("new_topic/", views.new_topic, name="new_topic"),
     # The page for adding a new entry.
     path("new_entry/<int:topic_id>/", views.new_entry, name="new_entry"),
+    # The page for editing and entry.
+    path("edit_entry/<int:entry_id>/", views.edit_entry, name="edit_entry"),
 ]
