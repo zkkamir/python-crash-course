417b6e20ffb6b17a70cdbed58f68d10ab4a55019
Amirh. Zahmatkesh Kenari <zkkamir@gmail.com>
2021-04-05 19:21:35 +0430

    Created 'adding entry' page.

--- learning_log/learning_logs/forms.py
+++ learning_log/learning_logs/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.db import models
 from django.db.models import fields
+from django.forms import widgets
 
-from .models import Topic
+from .models import Topic, Entry
 
 
 class TopicForm(forms.ModelForm):
@@ -9,3 +11,11 @@ class TopicForm(forms.ModelForm):
         model = Topic
         fields = ["text"]
         labels = {"text": ""}
+
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ["text"]
+        labels = {"text": "Entry:"}
+        widgets = {"text": forms.Textarea(attrs={"cols": 80})}
--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path("topics/<int:topic_id>/", views.topic, name="topic"),
     # The page for adding a new topic.
     path("new_topic/", views.new_topic, name="new_topic"),
+    # The page for adding a new entry.
+    path("new_entry/<int:topic_id>/", views.new_entry, name="new_entry"),
 ]
