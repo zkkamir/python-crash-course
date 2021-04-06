9c9284dfc4a91c31e1991eb58718e44e881860a5
Amirh. Zahmatkesh Kenari <zkkamir@gmail.com>
2021-04-06 17:05:19 +0430

    Restricted access to private pages to logged in users only.

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -9,6 +10,7 @@ def index(request):
     return render(request, "learning_logs/index.html")
 
 
+@login_required
 def topics(request):
     """Show all topics."""
     topics = Topic.objects.order_by("date_added")
@@ -17,6 +19,7 @@ def topics(request):
     return render(request, "learning_logs/topics.html", context)
 
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all it's entries."""
     topic = Topic.objects.get(id=topic_id)
@@ -25,6 +28,7 @@ def topic(request, topic_id):
     return render(request, "learning_logs/topic.html", context)
 
 
+@login_required
 def new_topic(request):
     """Add a new topic."""
     if request.method != "POST":
@@ -42,6 +46,7 @@ def new_topic(request):
     return render(request, "learning_logs/new_topic.html", context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -63,6 +68,7 @@ def new_entry(request, topic_id):
     return render(request, "learning_logs/new_entry.html", context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an exitsting entry."""
     entry = Entry.objects.get(id=entry_id)
